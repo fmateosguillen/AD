10240b6275e4777ee4b556a743c1888669d4d203
Francisco Javier Mateos Guillén <fmateosguillen@gmail.com>
2020-10-23 14:24:09 +0200

    Cambios 23/10 14:24

--- Ejercicio01Clase/src/main/java/com/example/Ejercicio01Clase/repos/ProductoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ejercicio01Clase.repos;
-
-import com.example.Ejercicio01Clase.modelo.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
-
-}
